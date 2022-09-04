036fb7974eca253d961433cc919790a0175bd064
Ahmed Abu Sharar <absam9945@gmail.com>
2022-09-04 20:28:44 +0300

    create sign in controllers relates #4

--- server/controllers/register/sign-in.js
+++ server/controllers/register/sign-in.js
@@ -1,7 +1,45 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 const { signInQuery } = require('../../database/queries');
+const { signInSchema } = require('../../validation');
 
 const signIn = (req, res, next) => {
+  const { email, password } = req.body;
+  const { error, value } = signInSchema.validate(req.body);
 
+  if (error) {
+    res.status(401).send({ message: error.details[0].message, state: 'fail' });
+  } else {
+    signInQuery(email)
+      .then((data) =>{
+        if (data.rows.length === 0) throw new Error('invalid email or password');
+        else {
+          const {
+            hashedpassword,
+            id,
+            name,
+            img,
+          } = data.rows[0];
+
+          bcrypt.compare(password, hashedpassword, (err, result) => {
+            if (err) next(err);
+            if (!result) res.json({ message: 'invalid email or password', state: 'fail' });
+            else {
+              const token = jwt.sign({
+                id,
+                name,
+                email,
+                img,
+              }, process.env.SECRET);
+              res.cookie('token', token, { httpOnly: true }).json({message: 'successful'});
+            }
+          });
+        }
+      })
+      .catch((err) => res.json({ message: `${err}`, state: 'fail' }));
+  }
 };
 
 module.exports = { signIn };
--- server/database/queries/register/sign-in.js
+++ server/database/queries/register/sign-in.js
@@ -1,5 +1,5 @@
 const { connection } = require('../../config/connection');
 
-const signInQuery = () => connection.query('');
+const signInQuery = (email) => connection.query('select id, name, password AS hashedPassword, img from users where email = ($1);', [email]);
 
 module.exports = { signInQuery };
