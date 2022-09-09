f52c59dd691f00b6ba737f376ab63a2a2072d999
Ahmed Abu Sharar <absam9945@gmail.com>
2022-09-09 17:19:51 +0300

    create delete post relates #29

--- server/controllers/posts/delete-post.js
+++ server/controllers/posts/delete-post.js
@@ -1,7 +1,10 @@
 const { deletePostQuery } = require('../../database/queries');
 
 const deletePost = (req, res, next) => {
-
+  const { id } = req.params;
+  deletePostQuery(id)
+    .then(res.send({ message: 'deleted successfully' }))
+    .catch((err) => next(err));
 };
 
 module.exports = { deletePost };
