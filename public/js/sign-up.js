72d5968c1c717199cde66885879113277b186f04
Ahmed Abu Sharar <absam9945@gmail.com>
2022-09-09 16:22:45 +0300

    create sign-up fetch relates #20

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -1,31 +1,34 @@
+const nickname = document.querySelector('.nickname');
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
-const login = document.querySelector('.login');
+const repeatPassword = document.querySelector('.repeatPassword');
+const signUp = document.querySelector('.signUp');
 const form = document.querySelector('#form');
 const span = document.querySelector('.span');
 span.style.color = 'red';
-const validation = (emailVal, passwordVal) => {
+const validation = (emailVal, passwordVal, repeatPasswordVal) => {
   const emailPattern = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
   const passwordPattern = /[A-Za-z0-9]/gi;
   if (!emailPattern.test(emailVal)) return 'invalid email';
   if (!passwordPattern.test(passwordVal) || passwordVal.length < 7) return 'your password must be at least 7 characters';
+  if (passwordVal !== repeatPasswordVal) return 'passwords don\'t match';
   return true;
 };
-login.addEventListener('click', (e) => {
+signUp.addEventListener('click', (e) => {
   e.preventDefault();
-  const val = validation(email.value, password.value);
-  if (!email.value || !password.value) {
+  const val = validation(email.value, password.value, repeatPassword.value);
+  if (!email.value || !password.value || !repeatPassword.value) {
     span.textContent = 'Please fill in all fields';
   } else if (val === true) {
-    fetch('/user/sign-in', {
+    fetch('/user/sign-up', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ email: email.value, password: password.value }),
+      body: JSON.stringify({ name: nickname.value, email: email.value, password: password.value }),
     }).then((res) => res.json())
       .then((data) => {
-        if (data.message === 'successful') window.location.href = '/feed';
+        if (data.state === 'success') window.location.href = '/feed';
         else {
           span.textContent = data.message;
         }
